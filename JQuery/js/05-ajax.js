17660eda9050d7564ff960c94ad7f332c972485b
rgpriede66 <personal@robgalpr.es>
2020-10-11 00:03:56 +0200

    Ajax

---
+++ JQuery/js/05-ajax.js
@@ -0,0 +1,32 @@
+'use strict'
+// ver web reqres.in
+$(document).ready(function($) {
+    //load
+    //$('#datos').load('https://reqres.in')
+    //get
+    $.get('https://reqres.in/api/unknown', function(response) {
+            console.log(response)
+            response.data.forEach((element, index) => {
+                $("#datos").append("<p>" + element.name + "</p>")
+            })
+        })
+        //post
+    let usuario = {
+        email: "eve.holt@reqres.in",
+        password: "pistol"
+    }
+    $.post('https://reqres.in/api/register', usuario, function(response) {
+        console.log(response)
+    })
+    $.ajax({
+        url: "https://reqres.in/api/users",
+        type: "POST",
+        data: {
+            name: "paul rudd",
+            movies: ["I Love You Man", "Role Models"]
+        },
+        success: function(response) {
+            console.log(response);
+        }
+    });
+})
