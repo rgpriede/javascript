d6e672ffb42e627ca23a57938a03378702185b68
rgpriede66 <personal@robgalpr.es>
2020-10-11 20:35:33 +0200

    FIN teoria

--- JQuery/js/06-interface.js
+++ JQuery/js/06-interface.js
@@ -1,5 +1,36 @@
 'use strict'
 $(document).ready(function() {
-    // console.log("loading...")
+    // mover elementos por la página
     $('.elemento').draggable()
+        // resizable
+    $('.elemento').resizable()
+        // selectable - sortable no funcionan conjuntamente
+        //$(".listaSeleccionable").selectable()
+    $(".listaSeleccionable").sortable()
+        //drop
+    $("#elementoMovido").draggable()
+    $("#area").droppable({
+            drop: function() {
+                console.log("has soltado en area")
+            }
+        })
+        //efectos
+    $("#mostrar").click(function() {
+            //$(".cajaEfectos").toggle('fade') //desaparece
+            //$(".cajaEfectos").toggle('explode') // explota en varias cachos
+            //$(".cajaEfectos").toggle('blink') // 'slide'  
+            //$(".cajaEfectos").toggle('drop') // 'puff' 'scale' 'shake'
+            //podemos pasar opciones o json
+            $(".cajaEfectos").toggle('shake', 'slow')
+        })
+        // tooltip
+    $("document").tooltip()
+    $("#lanzar").click(function() {
+            //dialog
+            $('#popup').dialog()
+        })
+        // añadir calendario
+    $('#calendario').datepicker()
+        // sistema de carpetas
+    $("#folder").tabs()
 })
